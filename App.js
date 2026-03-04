2529f315403e384a05b66a257da6114a74b4e795
Franchesco Jordan <jordanseugue@gmail.com>
2026-03-04 01:12:06 -0500

    Temporary way of deleting data of the Onboarding screens on the home screen(menu prinicpal);)

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import SplashScreen from './src/screens/SplashScreen';
-import OnboardingScreen from './src/screens/OnboardingScreen';
+import "react-native-gesture-handler";
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SplashScreen from "./src/screens/SplashScreen";
+import OnboardingScreen from "./src/screens/OnboardingScreen";
+
+import { clearAll } from "./src/services/storageService";
 
 const Stack = createStackNavigator();
 
 function HomePlaceholder() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Home - À venir ✅</Text>
+      <TouchableOpacity
+        onPress={async () => {
+          await clearAll();
+          alert("Données effacées !");
+        }}
+        style={{
+          marginTop: 20,
+          padding: 12,
+          backgroundColor: "#E63946",
+          borderRadius: 8,
+        }}
+      >
+        <Text style={{ color: "white" }}>🗑️ Reset Onboarding</Text>
+      </TouchableOpacity>
     </View>
   );
 }
